88ccbe53e1652b50e6eec8b337918fb19b8fe3bb
Copatych <xxxyy9817@gmail.com>
2021-05-18 00:23:26 +0700

    Scrollable keyboard was configured. Base keyboard was modified

--- app/src/main/java/copatych/engineeringcalculatormobile/Keyboards.kt
+++ app/src/main/java/copatych/engineeringcalculatormobile/Keyboards.kt
@@ -2,18 +2,20 @@ package copatych.engineeringcalculatormobile
 
 class Keyboards(private val calculatorApp: CalculatorApp = CalculatorApp()) {
 
-    // TODO Dynamicaly change keyboard
+    // TODO Dynamically change keyboard
     val base: List<List<String>> = listOf(
-        listOf(" sin ", " cos ", " tan ", " pi ", " abs "),
-        listOf(" round ", " ( ", " ) ", " e "),
+        listOf(" round ", " ( ", " ) ", " ; ", "e"),
         listOf("7", "8", "9", " / ", " ln "),
         listOf("4", "5", "6", " * ", " ^ "),
-        listOf("1", "2", "3", " - ", " max "),
-        listOf(" ! ", "0", " . ", " + ", " min "),
+        listOf("1", "2", "3", "-", " max "),
+        listOf(" ! ", "0", ".", " + ", " min "),
+        listOf(" sin ", " cos ", " tan ", " pi ", " abs "),
         listOf(" asin ", " acos ", " atan ", " if "),
     )
 
     val functions: List<String> = calculatorApp.allFunctionsNames
+        .map { if ((it != "e") and (it != "E")) " $it " else it }
 
     val operations: List<String> = calculatorApp.operationsDirector.operationsNames()
+        .map { if (it != "-") " $it " else it }
 }
